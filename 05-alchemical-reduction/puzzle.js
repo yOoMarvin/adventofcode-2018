09acafce8251c993a07067c7371c1bc91b8259de
yoomarvin <marvin.messenzehl@gmail.com>
2018-12-05 18:35:10 +0100

    day 05 - slow AF

--- 05-alchemical-reduction/puzzle.js
+++ 05-alchemical-reduction/puzzle.js
@@ -5,39 +5,56 @@ var _ = require("lodash");
 var fs = require("fs");
 var lines = fs.readFileSync("./input.txt", "utf8").split("\n");
 
-var polymer = lines[0];
-var count = 0;
-console.log("Inital Lenght:", polymer.length);
+var initialPolymer = lines[0];
 
-for (var i = 0; i < polymer.length; i++) {
-  var char = polymer[i];
-  var nextChar = polymer[i + 1];
-  if (nextChar != null) {
-    if (char == char.toLowerCase()) {
-      if (nextChar == nextChar.toUpperCase()) {
-        if (char.toLowerCase() == nextChar.toLowerCase()) {
-          //   polymer = polymer.slice(0, i - 1) + polymer.slice(i);
-          count++;
-          count++;
-          //console.log("Killed two chars: ", char, " and ", nextChar);
-          i++;
-        }
+// ===== HELPERS =====
+function reaction(polymer) {
+  polymer = polymer.split("");
+  // playing with fire here
+  while (true) {
+    didSomething = false;
+
+    for (let i = 0; i < polymer.length - 1; i++) {
+      // helper variables for the upcoming if. Check if the char is a upper or lower letter
+      let upper = polymer[i].toUpperCase();
+      let lower = polymer[i].toLowerCase();
+      // learning about ternary operator today!
+      //
+      if (
+        // first check the first part
+        // if this is true go the the thing after the ?
+        // if false then go to the part at the end
+        polymer[i] === lower
+          ? polymer[i + 1] === upper
+          : polymer[i + 1] === lower
+      ) {
+        polymer = polymer.slice(0, i).concat(polymer.slice(i + 2));
+        didSomething = true;
+        break;
       }
     }
-    if (char == char.toUpperCase()) {
-      if (nextChar == nextChar.toLowerCase()) {
-        if (char.toLowerCase() == nextChar.toLowerCase()) {
-          //   polymer = polymer.slice(0, i - 1) + polymer.slice(i);
-          count++;
-          count++;
-          //console.log("Killed two chars: ", char, " and ", nextChar);
-          i++;
-        }
-      }
-    }
+    if (!didSomething) break;
   }
+  return polymer.length;
 }
 
-console.log("Reacted: ", count);
+// ===== PART 1 =====
+console.log("REACTED POLYMER LENGTH:", reaction(initialPolymer));
 
-console.log("Remaining length: ", polymer.length - count);
+// ===== PART 2 ====
+
+// result object where everything is saved
+let table = {};
+
+// loop through alphabet
+for (let i = 0; i < 26; i++) {
+  // unicode small a and big A
+  let lowercase = String.fromCharCode(97 + i);
+  let uppercase = String.fromCharCode(65 + i);
+  // learn about regex magix today
+  regex = new RegExp("[" + lowercase + uppercase + "]", "g");
+  let tempInput = initialPolymer.replace(regex, "");
+  table[uppercase] = reaction(tempInput);
+}
+
+console.log(table);
